ce7207358e86672a460046637ea1eec8869da2f2
Junsu <murane@naver.com>
2021-01-30 04:23:21 +0900

    Initial commit

---
+++ src/test/java/newbie/jun/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package newbie.jun.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
